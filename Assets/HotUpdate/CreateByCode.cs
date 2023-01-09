2049407a69ba8b595ee08d6eefc2b142dfec86da
jacky <jackylvm@foxmail.com>
2023-01-09 15:10:40 +0800

    从hybridclr_trial项目拷贝一些内容到工程里面,减少创建的麻烦

---
+++ Assets/HotUpdate/CreateByCode.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CreateByCode : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("这个脚本是通过代码AddComponent直接创建的");
+    }
+}
---
+++ Assets/Main/AOTGenericReferences.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AOTGenericReferences : MonoBehaviour
+{
+    public void RefMethods()
+    {
+        // System.Object UnityEngine.AssetBundle::LoadAsset<System.Object>(System.String)
+        // System.Object UnityEngine.GameObject::AddComponent<System.Object>()
+    }
+}
